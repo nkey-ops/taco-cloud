c4405a5dbe3663a67ec3ac9361fcdb398e29697e
deuru <terrythebear746@gmail.com>
2023-04-18 14:45:47 +0400

    Added Domain of taco objects, DesignTacoController and design view

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Taco {
+
+	private String name;
+	
+	private List<Ingredient> ingredients;
+}
--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -2,6 +2,8 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -9,5 +11,12 @@ public class TacoCloudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
-
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+	    InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+//	    internalResourceViewResolver.setPrefix("pages/");
+	    internalResourceViewResolver.setSuffix(".html");
+	    return internalResourceViewResolver;
+	}
 }
