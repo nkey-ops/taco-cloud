5cd8ee803b49afc66f742ee910c94d1f547d2426
deuru <terrythebear746@gmail.com>
2023-06-10 22:19:36 +0400

    Initial Commit

---
+++ src/main/java/auth/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package auth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import tacos.data.UserRepository;
+
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+			throws Exception {
+		return http
+				.authorizeRequests(authorizeRequests ->
+				authorizeRequests.anyRequest().authenticated()
+						)
+				.formLogin()
+				.and().build();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService(UserRepository userRepo) {
+		return username -> userRepo.findByUsername(username).get();
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
